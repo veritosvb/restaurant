36d4e2e7be1bd32bc8bf5fb6a8e6d7df923d0b33
Veritosvb <vero.velez@gmail.com>
2018-07-08 17:26:34 -0500

    post and sort tables and waitlists

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,3 @@
-var path = require("path");
 var reservations = require("../data/tableData.js");;
 var waitList = require("../data/waitinglistData.js");;
 
@@ -8,51 +7,42 @@ var waitList = require("../data/waitinglistData.js");;
 // Basic route that sends the user first to the AJAX Page
 module.exports = function(app){
 // Displays all characters
-  app.get("/api/reservations", function(req, res) {
-    return res.json(characters);
-  });
-  
-  // Displays a single character, or returns false
-  app.get("/reservations/:reservations", function(req, res) {
+  app.get("/reservations", function(req, res) {
       //reservations idcode
-    var chosen = req.params.reservations;
-  
-    console.log(chosen);
     console.log(reservations.length);
-
-    //if more reservatioons available 
-
     if(reservations.length < 5){
         console.log("making reservation");
+        return res.json(true);
     }
     else{
-        console.log("adding to waiting list");
+        return res.json(false);
     }
   
   });
   
 
-  function makeReservation(){
-  app.post("/api/reservations", function(req, res) {
+
+  app.post("/reservations/:reservations", function(req, res) {
+      var choise = req.params.reservations;
+      console.log("Posting new table");
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
     var newTable = req.body;
   
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newTable.name = newTable.name.replace(/\s+/g, "").toLowerCase();
-  
     console.log(newTable);
-  
-    reservations.push(newTable);
-  
+
+    if(choise == "table"){
+        reservations.push(newTable);
+    }else{
+        waitList.push(newTable);
+    }
     res.json(newTable);
   });
 }
-  
-    
 
-}
+
+
+
 
 
 
